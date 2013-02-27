c82c753cc45c38ff09cccd91094d344101ea1efb
Dmitry Tsoy <hd.deman@gmail.com>
2013-02-27 13:03:19 +0400

    fix #1

--- lib/Werkint/Qiwi/Soap/Server.php
+++ lib/Werkint/Qiwi/Soap/Server.php
@@ -53,7 +53,7 @@ class Server extends \SoapServer
         $this->callback = null;
     }
 
-    public function updateBill(S\UpdateBillResponse $param)
+    public function updateBill(Werkint\Qiwi\ServerMethods\UpdateBillResponse $param)
     {
         // Проверки подписи
         if ($param->login != $this->login) {
